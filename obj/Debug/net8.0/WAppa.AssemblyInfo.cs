d4d5181ad86503a260b24074f69b19fe27f10b3e
miscaro <marianoiscaro5@gmail.com>
2025-05-07 18:07:56 +0200

    modifica gitignore

--- obj/Debug/net8.0/WAppa.AssemblyInfo.cs
+++ obj/Debug/net8.0/WAppa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAppa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025ab62891e10cd1d5d56606681e431616062e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1def41d72377c994e9826776e5d5d8be584432")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAppa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAppa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
